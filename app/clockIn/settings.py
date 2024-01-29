4ac68b6c7a80eb450fc711f0342185f4640a8f30
alexriggen <alejandro.riggencinta@student.manchester.ac.uk>
2024-01-29 11:02:36 +0000

    templates folders added inside base app and global app

--- app/clockIn/settings.py
+++ app/clockIn/settings.py
@@ -89,7 +89,9 @@ ROOT_URLCONF = 'clockIn.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'templates',
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
