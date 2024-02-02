3c8a899c26a4834f1042c9e6070c45779fcb937b
Patrick Jones <git@patrickjones.uk>
2024-02-02 23:31:29 +0000

    Integrate login design

--- app/base/forms.py
+++ app/base/forms.py
@@ -1,6 +1,8 @@
 
 from django import forms
 from django.contrib.auth.admin import UserChangeForm, UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm, UsernameField
+from django.utils.translation import gettext_lazy as _
 
 from base.models import User, Shift
 
@@ -28,4 +30,12 @@ class ShiftChangeForm(forms.ModelForm):
     class Meta:
         model = Shift
         fields = ('assigned_to', 'start_at', 'end_at', 'wage_multiplier')
+
+class LoginForm(AuthenticationForm):
+    username = UsernameField(widget=forms.TextInput(attrs={"autofocus": True, "class": "inputs", "placeholder": "email address", "type": "email"}))
+    password = forms.CharField(
+        label=_("Password"),
+        strip=False,
+        widget=forms.PasswordInput(attrs={"autocomplete": "current-password", "class": "inputs", "placeholder": "password"}),
+    )
 
--- app/base/views.py
+++ app/base/views.py
@@ -1,7 +1,10 @@
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout as auth_logout
-from django.http import HttpRequest, HttpResponse
+from django.contrib.auth.views import LoginView
+from django.http import HttpRequest
 from django.shortcuts import render
+
+from base.forms import LoginForm
 
 @login_required
 def profile(request: HttpRequest):
@@ -10,3 +13,6 @@ def profile(request: HttpRequest):
 def logout(request: HttpRequest):
     auth_logout(request)
     return render(request, 'registration/logged_out.html')
+
+class CustomLoginView(LoginView):
+    form_class = LoginForm
